da820c98b42bf7b13dfea382a994079159eda2a9
Ravisa Dilanka <raveeshadilanka1999@gmail.com>
2024-09-05 08:44:06 +0530

    Completed layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,35 +1,42 @@
 import React from "react";
-import { Flex, Layout, Menu } from "antd";
+import { Layout, Menu } from "antd";
 import { HomeOutlined, FileAddOutlined } from "@ant-design/icons";
 import "antd/dist/reset.css";
 import Link from "next/link";
 import { Typography } from "antd";
+import { useRouter } from "next/router";
 
 const { Title } = Typography;
-
 const { Header, Content } = Layout;
 
 const LayoutComponent: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
+
+  const router = useRouter()
+
+  const selectedKey = router.pathname === "/" ? "1" : "2"
+
   return (
     <Layout>
-      <Header className="header">
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["1"]} style={{display: "flex", justifyContent: "space-between"}}>
-          <div style={{display: "flex", alignItems:"center"}}>
-            <Title level={3} style={{ color: "white"}}>
-              My Task Manager
-            </Title>
-          </div>
-          <div>
-            <Menu.Item key="1" icon={<HomeOutlined />}>
-              <Link href="/">Home</Link>
-            </Menu.Item>
-            <Menu.Item key="2" icon={<FileAddOutlined />}>
-              <Link href="/add-task">Add Tasks</Link>
-            </Menu.Item>
-          </div>
+      <Header className="header" style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+        <div className="logo">
+          <Title level={3} style={{ color: "white", margin: 0 }}>
+            My Task Manager
+          </Title>
+        </div>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={[selectedKey]}
+          style={{ display: "flex", flexGrow: 1, justifyContent: "flex-end" }}
+        >
+          <Menu.Item key="1" icon={<HomeOutlined />}>
+            <Link href="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item key="2" icon={<FileAddOutlined />}>
+            <Link href="/add-task">Add Tasks</Link>
+          </Menu.Item>
         </Menu>
       </Header>
       <Content style={{ padding: "0 50px", height: "100vh" }}>
